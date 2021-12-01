1baedfd9bdfd1bdc10312259ca16c0b866376835
Michael Orvin <morvin1212@gmail.com>
2021-11-30 19:44:25 -0700

    Set up search bar, and form

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return(
+    <div>
+      <h1>Github Card</h1>
+      <form>
+        <input
+          type='text'
+          // id='search-bar'
+          // value={}
+          // onChange={}
+          placeholder='Search Git Handle'
+        />
+        <button>Search</button>
+      </form>
+    </div>
+    );
   }
 }
 
